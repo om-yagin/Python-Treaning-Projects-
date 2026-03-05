d9e6e8a1b83759d5f4804ea4b2feb62fd762a54c
Rahaf Elsidig <rahaf.elnile2009@gmail.com>
2026-03-05 17:03:12 +0300

    Add simple numbers summation script
    Initial implementation of a simple number summation script.

---
+++ Simple numbers summition.py
@@ -0,0 +1,10 @@
+numbers=[1,2,3,4,5]
+print(f"we have a number's lest of {numbers}")
+print("let's add each number to the next")
+x=0
+for y in numbers:
+    x+=y
+    print(f"+{y}")
+    
+    print(f"the total number is: {x}")
+    
